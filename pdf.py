7952939cc591ecec6245581b80e523e3f91467a9
fanbinbin <fanbinbin@kmf.com>
2018-02-23 17:18:36 +0800

    init

---
+++ pdf.py
@@ -0,0 +1,184 @@
+# coding=utf-8
+from __future__ import unicode_literals
+
+import logging
+import os
+import re
+import time
+
+
+try:
+    from urllib.parse import urlparse #py3
+except:
+    from urlparse import urlparse # py2
+
+import pdfkit
+import requests
+from bs4 import BeautifulSoup
+
+html_template = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+{content}
+</body>
+</html>
+
+"""
+
+class Crawler(object):
+    """
+    爬虫基类，所有爬虫都应该继承此类
+    """
+    name = None
+
+    def __init__(self, name, start_url):
+        """
+        初始化
+        :param name: 将要被保存的PDF的文件名称
+        :param start_url: 爬虫入口URL
+        """
+        self.name = name
+        self.start_url = start_url
+        self.domain = '{uri.scheme}://{uri.netloc}'.format(uri=urlparse(self.start_url))
+    @staticmethod
+    def request(url, **kwargs):
+        """
+        网络请求，返回response对象
+        :return: 
+        """
+        headers = {
+            "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding" : "gzip, deflate, br",
+            "Accept-Language" : "zh-CN,zh;q=0.9",
+            "Connection" : "keep-alive",
+            "Host" : "www.liaoxuefeng.com",
+            "Upgrade-Insecure-Requests" : "1",
+            "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+
+        }
+        time.sleep(5)
+        response = requests.get(url, headers = headers, **kwargs)
+        return response
+    
+    def parse_menu(self, response):
+        """
+        从response中解析所有目录URL链接
+        """
+        raise NotImplementedError
+    
+    def parse_body(self, response):
+        """
+        解析正文，由子类实现
+        :param response: 爬虫返回的response对象
+        :return: 返回经过处理的HTML正文文本
+        """
+
+        raise NotImplementedError
+
+    def run(self):
+        start = time.time()
+        options = {
+            'page-size': 'Letter',
+            'margin-top': '0.75in',
+            'margin-right': '0.75in',
+            'margin-bottom': '0.75in',
+            'margin-left': '0.75in',
+            'encoding': 'UTF-8',
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'cookie': [
+                ('cookie-name1', 'cookie-value1'),
+                ('cookie-name2', 'cookie-value2'),
+            ],
+            'outline-depth': 10,
+        }
+        htmls = []
+        urls = self.parse_menu(self.request(self.start_url))
+        # urls = list(urls)
+        # print(urls)
+        # print(self.parse_menu(self.request(self.start_url)))
+        for index, url in enumerate(self.parse_menu(self.request(self.start_url))):
+            # print(index)
+            # print(url)
+            html = self.parse_body(self.request(url))
+            f_name = ".".join([str(index), "html"])
+            # print(f_name)
+            # break
+            with open(f_name, 'wb') as f:
+                f.write(html)
+            htmls.append(f_name)
+
+        # path_wk = r'D:\Program\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        # config = pdfkit.configuration(wkhtmltopdf = path_wk)
+        # pdfkit.from_file(htmls, self.name + ".pdf", options = options, configuration=config)
+        # for html in htmls:
+        #     os.remove(html)
+        total_time = time.time() - start
+        print(u"总共耗时: %f 秒" % total_time)
+        print(htmls)
+
+class LiaoxuefengPythonCrawler(Crawler):
+    """
+    廖雪峰Python3教程
+    """
+    def parse_menu(self, response):
+        """
+        解析目录结构,获取所有URL目录列表
+        :param response 爬虫返回的response对象
+        :return: url生成器
+        """
+        soup = BeautifulSoup(response.content, "html.parser")
+        menu_tag = soup.find_all(class_ = "uk-nav uk-nav-side")[1]
+        # return menu_tag
+        for div in menu_tag.find_all("div"):
+            url = div.a.get("href")
+            if not url.startswith("http"):
+                url = "".join([self.domain, url]) # 补全为全路径
+            yield url
+
+    def parse_body(self, response):
+        """
+        解析正文
+        :param response: 爬虫返回的response对象
+        :return: 返回处理后的html文本
+        """
+        
+        try:
+            soup = BeautifulSoup(response.content, "html.parser")
+            body = soup.find_all(class_ = "x-wiki-content")[0]
+
+            # 加入标题, 居中显示
+            title = soup.find('h4').get_text()
+            center_tag = soup.new_tag("center")
+            title_tag = soup.new_tag('h1')
+            title_tag.string = title
+            center_tag.insert(1, title_tag)
+
+            body.insert(1, center_tag)
+
+            html = str(body)
+            # body中的img标签的src相对路径改为绝对路径
+            pattern = "(<img .*?src=\")(.*?)(\")"
+
+            def func(m):
+                if not m.group(2).startswith("http"):
+                    rtn = "".join([m.group(1), self.domain, m.group(2), m.group(3)])
+                    return rtn
+                else:
+                    return "".join([m.group(1), m.group(2), m.group(3)])
+            html = re.compile(pattern).sub(func, html)
+            html = html_template.format(content = html)
+            html = html.encode("utf-8")
+            return html
+        except Exception as e:
+            logging.error("解析错误", exc_info = True)
+            
+if __name__ == '__main__':
+    start_url = 'https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000'
+    crawler = LiaoxuefengPythonCrawler('廖雪峰Git', start_url)
+    crawler.run()
